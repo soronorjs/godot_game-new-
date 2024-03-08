1b205db02d09661a0cf6aa249fb091ee0e3e5301
soronorjs <adamflodman@flodmans.se>
2024-03-08 11:51:03 +0100

    a

---
+++ GameObjects/Enemies/KnightController.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var nav: NavigationAgent2D = $NavigationAgent2D
+@onready var Player: CharacterBody2D = $"../CharacterBody2D"
+
+func _physics_process(delta):
+	
+	var direction = Vector2()
+	
+	nav.target_position = Player.transform.get_origin()
+	
+	direction = nav.get_next_path_position() - global_position
+	direction = direction.normalized()
+	print(global_position)
+	
+	velocity = velocity.lerp(direction*SPEED, 2*delta)
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	
+
+	move_and_slide()
