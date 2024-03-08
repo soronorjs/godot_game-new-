d1bc5c3b139dde2210bdb0b05901ee4e12100b2e
soronorjs <adamflodman@flodmans.se>
2024-03-08 17:32:45 +0100

    Made pathfinding (kinda) work

--- GameObjects/Enemies/KnightController.gd
+++ GameObjects/Enemies/KnightController.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 3000.0
+const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -20,8 +20,8 @@ func _physics_process(delta):
 	
 	velocity = velocity.lerp(direction*SPEED, 7*delta)
 	
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	#if not is_on_floor():
+		#velocity.y += gravity * delta
 	
 	
 
