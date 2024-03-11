453028f2b07e97c6004d7bf0d6dc6200b1e20931
soronorjs <adamflodman@flodmans.se>
2024-03-11 13:33:17 +0100

    .

---
+++ Scenes/Rainbow.gd
@@ -0,0 +1,12 @@
+extends DirectionalLight2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$".".color = color.from_hsv(randf_range(0, 1), 1, 1, 1)
+	print($".".color)
