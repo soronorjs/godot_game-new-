345ffafa6e84bc1782c17c638d611a13d664381f
soronorjs <adamflodman@flodmans.se>
2024-03-15 09:14:20 +0100

    Implemented ShapeCast2D

--- GameObjects/Enemies/KnightController.gd
+++ GameObjects/Enemies/KnightController.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 30.0
-const JUMP_VELOCITY = -200.0
+const JUMP_VELOCITY = -250.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
@@ -13,14 +13,8 @@ var safeVelocity
 var rayDirections = []
 var lines = []
 
-func _ready():
-	for i in range(20):
-		var lineClone = line.duplicate()
-		lines.append(lineClone)
-		add_sibling(lineClone)
-
 func _physics_process(delta):
-	
+
 	var target_velocity = Vector2(30, 0) * global_position.direction_to(Player.position)
 	velocity.x = target_velocity.x
 
@@ -28,24 +22,27 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
-	for i in range(20):
-		
-		var space_state = get_world_2d().direct_space_state
-		var query = PhysicsRayQueryParameters2D.create($".".position, $".".position.rotated(deg_to_rad(18.0*i)))
-		query.exclude = [$"."]
-		var result = space_state.intersect_ray(query)
-		
-		print(Vector2.RIGHT.rotated(deg_to_rad(18.0*i)))
-		
-		if result:
-			var lineClone = lines[i]
-			lineClone.set_point_position(0, $".".position)
-			lineClone.set_point_position(1, result.position)
-			add_sibling(lineClone)
-			
+	var space_state = get_world_2d().direct_space_state
+	var shapeCast = $ShapeCast2D
+	
+	if(shapeCast.is_colliding()):
+		var collisionAmount = shapeCast.get_collision_count()
+		for i in range(collisionAmount):
+			if(shapeCast.get_collider(i).get_class() == "StaticBody2D"):
+				print(global_position.distance_to(shapeCast.get_collider(i).position))
+				if(global_position.distance_to(shapeCast.get_collider(i).position) <= 61.0) and (shapeCast.get_collider(i).position.y >= $".".position.y):
+					print(shapeCast.get_collider(i).position.y)
+					print($".".position.y)
+					_jump()
+	
 	
 
 	move_and_slide()
+	
+func _jump():
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		move_and_slide()
 	
 #func _on_navigation_agent_2d_velocity_computed(safe_velocity):
 	#velocity = safe_velocity
