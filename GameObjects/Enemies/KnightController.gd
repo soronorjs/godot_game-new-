08975f93e74470621ad36e4797c3267f3b522264
soronorjs <adamflodman@flodmans.se>
2024-03-19 13:31:37 +0100

    Bug fixes

--- GameObjects/Enemies/KnightController.gd
+++ GameObjects/Enemies/KnightController.gd
@@ -7,8 +7,9 @@ const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
 @onready var Player: CharacterBody2D = $"../CharacterBody2D"
-@onready var line: Line2D = $"../Line2D"
-@onready var shapeCast: ShapeCast2D = $ShapeCast2D
+@onready var line: Line2D = $PlaceholderEnemy/Line2D
+@onready var shapeCast: ShapeCast2D = $PlaceholderEnemy/ShapeCast2D
+@onready var rayCast: RayCast2D = $PlaceholderEnemy/RayCast2D
 
 var safeVelocity
 
@@ -17,26 +18,29 @@ var lines = []
 var Direction = Vector2.LEFT.x
 
 func _physics_process(delta):
+	
+	velocity.x = Direction * SPEED
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
 	var space_state = get_world_2d().direct_space_state
-			
-	if not is_on_floor():
-		velocity.x = Direction * SPEED
-		print("Idiot")
 	
 	if(shapeCast.is_colliding()):
 		var collisionAmount = shapeCast.get_collision_count()
 		for i in range(collisionAmount):
 			if(shapeCast.get_collider(i).get_class() == "StaticBody2D"):
-				print(global_position.distance_to(shapeCast.get_collider(i).position))
 				if(global_position.distance_to(shapeCast.get_collider(i).position) <= 59.0) and (shapeCast.get_collider(i).position.y >= $".".position.y):
-					print(shapeCast.get_collider(i).position.y)
-					print($".".position.y)
 					_jump()
-	
+					
+	print(rayCast.is_colliding())
+	if not rayCast.is_colliding():
+		Direction *= -1
+		
+		rayCast.position.x *= Direction
+		$PlaceholderEnemy.scale *= Direction
+		line.set_point_position(0, rayCast.position)
+		line.set_point_position(1, rayCast.target_position)
 	
 
 	move_and_slide()
@@ -50,13 +54,3 @@ func _jump():
 	#velocity = safe_velocity
 	#print(safe_velocity)
 	move_and_slide()
-
-
-func _on_area_2d_2_body_exited(body):
-	while not shapeCast.is_colliding():
-		if Direction == Vector2.LEFT.x:
-			Direction = Vector2.RIGHT.x
-			break
-		elif Direction == Vector2.RIGHT.x:
-			Direction == Vector2.LEFT.x
-			break
