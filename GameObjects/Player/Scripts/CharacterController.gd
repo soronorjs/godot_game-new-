93b984eca940017b89d3b740d50ef9fd9cd6a552
soronorjs <adamflodman@flodmans.se>
2024-03-28 14:41:21 +0100

    Bug fixes

--- GameObjects/Player/Scripts/CharacterController.gd
+++ GameObjects/Player/Scripts/CharacterController.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var Dashing = $".".get_meta(&"Dashing")
+
 func _physics_process(delta):
 	
 	# Gravity control
