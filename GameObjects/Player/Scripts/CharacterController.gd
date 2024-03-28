0fdfa02a2924297ac9354e89d8b608d9c4f1e985
soronorjs <adamflodman@flodmans.se>
2024-03-28 12:00:29 +0100

    Dash System Started

--- GameObjects/Player/Scripts/CharacterController.gd
+++ GameObjects/Player/Scripts/CharacterController.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
-@onready var Player_Base: CharacterBody2D = get_node("%Player")
-@onready var Player_Sprite: AnimatedSprite2D = get_node("Player_Sprite")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	
@@ -16,20 +15,24 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-		
 
 	# Walking Controls
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
+	if Input.is_action_just_pressed("Dash"):
+		if $Player_Sprite.flip_h == true:
+			%Player.position.x += 50 * -1
+		else:
+			%Player.position.x += 50 * 1
+	
 	if direction:
 		velocity.x = direction * SPEED
 		if direction == -1:
-			Player_Sprite.flip_h = true
+			$Player_Sprite.flip_h = true
 		else:
-			Player_Sprite.flip_h = false
+			$Player_Sprite.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		Player_Sprite.play("Idle")
+		$Player_Sprite.play("Idle")
 
 	move_and_slide()
