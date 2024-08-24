58196b7738b79367a5be9518b0e4d262046babd8
soronorjs <adamflodman@flodmans.se>
2024-08-24 03:17:52 +0200

    Scene Transitions Working

--- GameObjects/GlobalScripts/SceneController.gd
+++ GameObjects/GlobalScripts/SceneController.gd
@@ -9,8 +9,10 @@ func _ready():
 	load_scene("res://Scenes/MainMenu.tscn")
 
 func load_scene(scene_path: String):
-	main_animation.play("TransitionScreen")
+	main_animation.play_backwards("TransitionScreen")
 	
+	while main_animation.is_playing() and main_animation.current_animation == "TransitionScreen" and main_animation.get_playing_speed() == -1.0:
+		await get_tree().process_frame
 	if scene_holder.get_child_count() > 0:
 		for child in scene_holder.get_children():
 			scene_holder.remove_child(child)
@@ -20,7 +22,6 @@ func load_scene(scene_path: String):
 	if scene:
 		var scene_instance = scene.instantiate()
 		scene_holder.add_child(scene_instance)
-		call_deferred("scene_loaded", scene_instance)
 		
 		var scene_cam = scene_instance.get_node("Player/Cameras/MainCam")
 	
@@ -29,14 +30,10 @@ func load_scene(scene_path: String):
 			print(get_viewport().get_camera_2d())
 		else:
 			push_error("Failed to recognize camera")
+			
+		scene_loaded(scene_instance)
 	else:
 		print("Failed to load scene: ", scene_path)
 		
-func scene_loaded():
-	print("Works!")
+func scene_loaded(scene):
 	main_animation.play("TransitionScreen")
-
-
-func _on_animation_player_animation_finished(anim_name):
-	if SceneManager.get_node("CanvasLayer/SceneTransition").color == Color.BLACK:
-		main_animation.play("TransitionScreen")
