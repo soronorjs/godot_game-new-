dac88445d4405bc2f8d5d4f81b399a2fabcbf17b
soronorjs <adamflodman@flodmans.se>
2024-08-25 17:53:28 +0200

    Began Work on Audio Transition

--- GameObjects/GlobalScripts/SceneController.gd
+++ GameObjects/GlobalScripts/SceneController.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 @onready var scene_holder = get_node("/root/SceneManager/SceneHolder")
 @onready var main_audio = get_node("/root/SceneManager/MainAudio")
+@onready var main_volume_db = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
 @onready var main_animation = SceneManager.get_node("AnimationPlayer")
+
+var tweenSound
 
 func _ready():
 	SceneManager.get_node("CanvasLayer/SceneTransition").visible = true
+	main_audio.volume_db = 0.0
 	load_scene("res://Scenes/MainMenu.tscn")
 
 func load_scene(scene_path: String):
@@ -14,6 +18,22 @@ func load_scene(scene_path: String):
 	
 	while main_animation.is_playing() and main_animation.current_animation == "TransitionScreen" and main_animation.get_playing_speed() == -1.0:
 		await get_tree().process_frame
+		
+	if scene_path != "res://Scenes.MainMeny.tscn":
+		tweenSound = get_tree().create_tween()
+		tweenSound.tween_method(Callable(self, "set_bus_volume_db"), main_volume_db, -80.0, 3)
+		tweenSound.play()
+		main_audio.stop()
+	elif scene_path == "res://Scenes.MainMeny.tscn":
+		main_audio.stop()
+		main_audio.play(0.0)
+		main_audio.seek(0.0)
+		tweenSound = get_tree().create_tween()
+		tweenSound.tween_method(Callable(self, "set_bus_volume_db"), main_volume_db, 0.0, 3)
+		tweenSound.play()
+		
+	await tweenSound.finished
+		
 	if scene_holder.get_child_count() > 0:
 		for child in scene_holder.get_children():
 			scene_holder.remove_child(child)
@@ -38,3 +58,7 @@ func load_scene(scene_path: String):
 		
 func scene_loaded(scene):
 	main_animation.play("TransitionScreen")
+	
+func set_bus_volume_db(volume_db):
+	var bus_idx = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bus_idx, volume_db)
--- GameObjects/Main Menu/Play.gd
+++ GameObjects/Main Menu/Play.gd
@@ -7,20 +7,15 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	AudioPlayer.play()
 	$".".pressed.connect(self._button_pressed)
 
 func _button_pressed():
-	var tweenSound = get_tree().create_tween()
-	tweenSound.tween_method(Callable(self, "set_bus_volume_db"), 0.0, -20.0, 3)
-	
 	SceneController.load_scene("res://Scenes/TestScene.tscn")
-
-func set_bus_volume_db(volume_db):
-	var bus_idx = AudioServer.get_bus_index("Master")
-	AudioServer.set_bus_volume_db(bus_idx, volume_db)
 	
 func _physics_process(delta):
-	if AudioServer.get_bus_volume_db(0) == -20.0:
+	if AudioServer.get_bus_volume_db(0) == -80.0:
 		AudioServer.set_bus_volume_db(0, 0.0)
-		AudioPlayer.playing = false
+		#AudioPlayer.playing = false
+		AudioPlayer.stop()
 		
